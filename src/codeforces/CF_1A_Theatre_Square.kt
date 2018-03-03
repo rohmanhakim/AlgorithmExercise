d7c735073b0210a53ceb9fb4ec05531eb3f5b4f1
rohmanhakim <rohmanhakim@live.com>
2018-03-03 13:48:53 +0700

    FIx solution for CF_1A and CF_230A, also rename the files

--- src/codeforces/CF_1A_Theatre_Square.kt
+++ src/codeforces/CF_1A_Theatre_Square.kt
@@ -2,18 +2,14 @@ package codeforces
 
 import java.util.*
 
-/**
- * Created by rohmanhakim on 3/2/18.
- */
-
 // http://codeforces.com/problemset/problem/1/A
-fun leastFlagStones(squareLength: Int, paveLength: Int): Int =
+fun leastFlagStones(squareLength: Long, paveLength: Long): Long =
         if((squareLength % paveLength) > 0) (squareLength / paveLength) + 1 else (squareLength / paveLength)
 
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
-    val n = nextInt()
-    val m = nextInt()
-    val a = nextInt()
+    val n = nextLong()
+    val m = nextLong()
+    val a = nextLong()
 
     println(leastFlagStones(n,a) * leastFlagStones(m,a))
 }
--- src/codeforces/CF_230A_Dragons.kt
+++ src/codeforces/CF_230A_Dragons.kt
@@ -2,34 +2,32 @@ package codeforces
 
 import java.util.*
 
-/**
- * Created by rohmanhakim on 3/1/18.
- */
-
 // http://codeforces.com/problemset/problem/230/A
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     var s = nextInt()
     val n = nextInt()
 
-    val x = mutableListOf<Int>()
-    val y = mutableListOf<Int>()
+    val x = mutableListOf<Pair<Int, Int>>()
 
     (1..n).forEach {
         val xi = nextInt()
         val yi = nextInt()
-        x.add(xi)
-        y.add(yi)
+        x.add(Pair(xi, yi))
     }
 
+    x.sortBy { it.first }
+
     (0..(n-1)).forEach { i ->
-        if(s < x[i]) {
+        if(s <= x[i].first) {
             println("NO")
             return@with
-        } else if(s > x[i] && i == (n-1)){
+        } else {
+            s += x[i].second
+        }
+
+        if(s > x[i].first && i == (n-1)){
             println("YES")
             return@with
         }
-
-        s += y[i]
     }
 }
